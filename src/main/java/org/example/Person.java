be7c8eadfaff7abd5de7c84889c06a8617a8a2b1
pablofs02 <pablofs02@tutanota.com>
2023-02-15 16:05:20 +0100

    Punto de partida

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import java.util.List;
+
+/**
+ * Class representing a person with a name, age and gender,
+ *
+ * @author Pablo Fernández Serrano
+ */
+
+public class Person {
+
+	private final String name;
+
+	private final int age;
+
+	private final String gender; // Male, Female
+
+	/**
+	 * Constructs a person with a name, age and gender.
+	 *
+	 * @param name the name of the person
+	 * @param age the age of the person
+	 * @param gender the gender of the person
+	 */
+
+	public Person(String name, int age, String gender) {
+		;
+	}
+
+	public String name() {
+		return name;
+	}
+
+	public int age() {
+		return age;
+	}
+
+	public String gender() {
+		return gender;
+	}
+
+	/**
+	 * Computes the average age of male and female persons in a list and returns the result in
+	 * an array of two elements (the first element is the male mean age and the second one is the * female mean age)
+	 *
+	 * @param persons
+	 * @return
+	 */
+
+	public double[] averageAgePerGender (List<Person> persons) {
+		return null;
+	}
+}
