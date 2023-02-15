3f17ad72295e35a3a38440679cd33b85f920f226
pablofs02 <pablofs02@tutanota.com>
2023-02-15 18:08:14 +0100

    Test del método averageAgePerGender

--- src/test/java/org/example/PersonTest.java
+++ src/test/java/org/example/PersonTest.java
@@ -2,6 +2,9 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -35,5 +38,78 @@ public class PersonTest {
 		assertThrows(RuntimeException.class, () ->
 						new Person("Juan", 50, "malee"),
 				"Gender unknown.");
+	}
+
+	@Test
+	void noFemale() {
+		List<Person> l = new ArrayList<Person>();
+		l.add(new Person("a",2,"male"));
+		l.add(new Person("b",8,"male"));
+		l.add(new Person("c",4,"male"));
+		double obtainedValueMale = Person.averageAgePerGender(l)[0];
+		double obtainedValueFemale = Person.averageAgePerGender(l)[1];
+		double expectedValueMale = 4.66666667;
+		double expectedValueFemale = 0;
+		assertEquals(expectedValueMale, obtainedValueMale, 0.001);
+		assertEquals(expectedValueFemale, obtainedValueFemale, 0.001);
+	}
+
+	@Test
+	void noMale() {
+		List<Person> l = new ArrayList<Person>();
+		l.add(new Person("a",2,"female"));
+		l.add(new Person("b",8,"female"));
+		l.add(new Person("c",4,"female"));
+		double obtainedValueMale = Person.averageAgePerGender(l)[0];
+		double obtainedValueFemale = Person.averageAgePerGender(l)[1];
+		double expectedValueMale = 0;
+		double expectedValueFemale = 4.66666667;
+		assertEquals(expectedValueMale, obtainedValueMale, 0.001);
+		assertEquals(expectedValueFemale, obtainedValueFemale, 0.001);
+	}
+
+	@Test
+	void mixGender() {
+		List<Person> l = new ArrayList<Person>();
+		l.add(new Person("a",3,"male"));
+		l.add(new Person("b",47,"female"));
+		l.add(new Person("c",32,"male"));
+		l.add(new Person("d",11,"female"));
+		l.add(new Person("e",13,"female"));
+		double obtainedValueMale = Person.averageAgePerGender(l)[0];
+		double obtainedValueFemale = Person.averageAgePerGender(l)[1];
+		double expectedValueMale = 17.5;
+		double expectedValueFemale = 23.66666667;
+		assertEquals(expectedValueMale, obtainedValueMale, 0.001);
+		assertEquals(expectedValueFemale, obtainedValueFemale, 0.001);
+	}
+
+
+	@Test
+	void bigAgeValues() {
+		List<Person> l = new ArrayList<Person>();
+		l.add(new Person("a",915349731,"male"));
+		l.add(new Person("b",851974505,"male"));
+		l.add(new Person("c",415496151,"male"));
+		double obtainedValueMale = Person.averageAgePerGender(l)[0];
+		double obtainedValueFemale = Person.averageAgePerGender(l)[1];
+		double expectedValueMale = 727606795.7;
+		double expectedValueFemale = 0;
+		assertEquals(expectedValueMale, obtainedValueMale, 0.1);
+		assertEquals(expectedValueFemale, obtainedValueFemale, 0.1);
+	}
+
+	@Test
+	void zeroAgeValues() {
+		List<Person> l = new ArrayList<Person>();
+		l.add(new Person("a",0,"male"));
+		l.add(new Person("b",0,"male"));
+		l.add(new Person("c",0,"female"));
+		double obtainedValueMale = Person.averageAgePerGender(l)[0];
+		double obtainedValueFemale = Person.averageAgePerGender(l)[1];
+		double expectedValueMale = 0;
+		double expectedValueFemale = 0;
+		assertEquals(expectedValueMale, obtainedValueMale, 0.001);
+		assertEquals(expectedValueFemale, obtainedValueFemale, 0.001);
 	}
 }
